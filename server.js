7824b1b04043a4ca9d71c3d114d359b5baf06488
AugmentedFifth <zcomito@gmail.com>
2017-06-10 00:04:11 -0700

    fixed scrollbars

--- server.js
+++ server.js
@@ -149,7 +149,7 @@ const codeSnippetSelection = [
     '("!-",,"-!")↵[0..2\n-- [("!-", 0, "-!"), ("!-", 1, "-!"), ("!-", 2, "-!")]',
     "Σ$(Nn/)↵[1..Nn\n-- Sum harmonic series for\n-- n = `command-line argument` :: String",
     "r=[0..9\n['|':(r≫=\\i→[|(iΔb,jΔc)∈[1,2,0,1,2]⊠[0,1,2,2,2]→'*'|→'_','|'])¦j←r\n-- Emit a 'Game of Life' glider on a 10x10 grid",
-    "n=[0..7\nPS⤔[[H$((i,j)∈)⊙V(\\s_→A(\\(z,w)->z∈n&&w∈n)$(\\(a,b)→F(\\c d→(a+c,b+d))[1,1,-1,-1,-2,-2,2,2][2,-2,2,-2,1,-1,1,-1])=≪s)[(Nb,Nc)]n¡0+48¦j←n]¦i←n\n-- Full program printing map of number of moves required\n-- by a knight to arrive at all places on the chessboard\n-- starting from the coordinates supplied as arguments"
+    "n=[0..7\nPS⤔[[H$((i,j)∈)⊙V(\\s_→A(\\(z,w)→z∈n&&w∈n)$(\\(a,b)→F(\\c d→(a+c,b+d))[1,1,-1,-1,-2,-2,2,2][2,-2,2,-2,1,-1,1,-1])=≪s)[(Nb,Nc)]n¡0+48¦j←n]¦i←n\n-- Full program printing map of number of moves required\n-- by a knight to arrive at all places on the chessboard\n-- starting from the coordinates supplied as arguments"
 ];
 
 function fisherYates(array) {
@@ -258,8 +258,8 @@ app.get("/editor", (req, res) => {
 app.get("/sch", (req, res) => {
     const filePath = path.join(__dirname, "public", "sch");
 
-    res.set("Content-disposition", "attachment; filename=sch");
-    res.set("Content-type", "text/javascript");
+    res.set("Content-Disposition", "attachment; filename=sch");
+    res.set("Content-Type", "text/javascript");
 
     const fileStream = fs.createReadStream(filePath);
     fileStream.pipe(res);
